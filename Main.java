b3f72323be94173c60abfeaa7eed360e984e457c
fafon41 <33590028+fafon41@users.noreply.github.com>
2017-11-13 15:41:57 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taptap;
+
+import javafx.scene.layout.Pane;
+
+/**
+ *
+ * @author Mac
+ */
+public class Main {
+    public Pane getPane(){
+        Pane pane = new Pane();
+        
+        return pane;
+    }
+    
+}
